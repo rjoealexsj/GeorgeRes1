6846119ffbb363a27f60795e9970fdeedd2fabca
george <georgevictorsj@gmail.com>
2022-01-13 20:19:47 +0530

    experimant 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import img1 from'./geo2/georgestand.jpg';
+import img2 from'./geo/jesus.jpg';
+import img3 from'./geo2/george.jpg';
 
 
 
@@ -11,13 +13,17 @@ function App() {
      <img src={img1} style={{width:'100px'}} />
 
      <center> <h2 style={{color:'blue'}}>Jesus 100px</h2> 
-     <img src={img1} style={{width:'200px', border:'20px solid yellow', borderRadius:'50%'}} />
+     <img src={img2} style={{width:'200px', border:'20px solid yellow', borderRadius:'50%'}} />
      </center>
 
      <div style={{textAlign:'right'}}>
      <h2 style={{color:'red',fontSize:'120px'}}>Jesus 100px</h2>
-     <img src={img1} style={{width:'300px',borderRadius:'20%'}} />
+     <img src={img3} style={{width:'300px',borderRadius:'20%'}} />
      </div>
+      <h2 style={{color:'blue'}}>Jesus 100px</h2> 
+     <img src={img3} style={{width:'350px', border:'20px solid green', borderRadius:'40%'}} />
+     
+    
       
    </div>
 
