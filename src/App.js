43536086d5e4e3a514f6c235b0729110621b7e29
george <georgevictorsj@gmail.com>
2022-01-13 15:33:33 +0530

    pro 1

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import img1 from'./geo/jesus.jpg';
 
 function App() {
   return (
-   <div>
-     <center>
-     <img src={img1} style={{width:'400px'}} />
+   <div style={{backgroundColor:'black'}}>
+     <h2 style={{color:'white'}}>Jesus 100px</h2>
+     <img src={img1} style={{width:'100px'}} />
+
+     <center> <h2 style={{color:'blue'}}>Jesus 100px</h2> 
+     <img src={img1} style={{width:'200px', border:'20px solid yellow', borderRadius:'50%'}} />
      </center>
+
+     <div style={{textAlign:'right'}}>
+     <h2 style={{color:'red',fontSize:'120px'}}>Jesus 100px</h2>
+     <img src={img1} style={{width:'300px',borderRadius:'20%'}} />
+     </div>
+     
      
 
    </div>
