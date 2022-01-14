086a6182b568e045d88542f671f8a2834ca215a2
george <georgevictorsj@gmail.com>
2022-01-14 15:57:28 +0530

    VJ2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import IDNavbar from './components/home';
+import IDNavbar from './components/IDNavbar';
 //import Home from './components/home';
 import {Col,FormGroup,Input,Label,Row,ButtonDropdown,DropdownToggle,DropdownMenu,DropdownItem} from 'reactstrap'; 
 import Home from './components/Home';
@@ -13,7 +13,7 @@ import Home from './components/Home';
 function App() {
   return (
    <div className='container' style={{paddingtop: '50px'}}>
-
+<IDNavbar/>
  <Home/>
 </div>
 
---
+++ src/components/IDNavbar.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand, NavabarToggler, NavItem, NavLink, DropdownItem, Nav, Collapse, NavbarText, DropdownToggle, DropdownMenu, NavbarToggler, UncontrolledDropdown, } from 'reactstrap'
+
+export default class IDNavbar extends Component {
+    render() {
+        return (
+            <div>
+               <div>
+  <Navbar
+    color="dark"
+    dark
+    expand="md"
+    fixed=""
+    full
+  >
+    <NavbarBrand href="/">
+      reactstrap
+    </NavbarBrand>
+    <NavbarToggler onClick={function noRefCheck(){}} />
+    <Collapse navbar>
+      <Nav
+        className="me-auto"
+        navbar
+      >
+        <NavItem>
+          <NavLink href="/components/">
+            Components
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/reactstrap/reactstrap">
+            GitHub
+          </NavLink>
+        </NavItem>
+        <UncontrolledDropdown
+          inNavbar
+          nav
+        >
+          <DropdownToggle
+            caret
+            nav
+          >
+            Options
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Option 1
+            </DropdownItem>
+            <DropdownItem>
+              Option 2
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Reset
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+      <NavbarText>
+        Simple Text
+      </NavbarText>
+    </Collapse>
+  </Navbar>
+</div> 
+            </div>
+        )
+    }
+}
