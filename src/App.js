49c62f66537a8d07e416bcafd46ac28c9ec1cd30
george <georgevictorsj@gmail.com>
2022-01-14 15:26:52 +0530

    vj

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import img1 from'./geo2/georgestand.jpg';
-import img2 from'./geo/jesus.jpg';
-import img3 from'./geo2/george.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import IDNavbar from './components/home';
+//import Home from './components/home';
+import {Col,FormGroup,Input,Label,Row,ButtonDropdown,DropdownToggle,DropdownMenu,DropdownItem} from 'reactstrap'; 
+import Home from './components/Home';
+
+
 
 
 
 function App() {
   return (
-   <div style={{backgroundColor:'black'}}>
-     <h2 style={{color:'white'}}>Jesus 100px</h2>
-     <img src={img1} style={{width:'100px'}} />
+   <div className='container' style={{paddingtop: '50px'}}>
 
-     <center> <h2 style={{color:'blue'}}>Jesus 100px</h2> 
-     <img src={img2} style={{width:'200px', border:'20px solid yellow', borderRadius:'50%'}} />
-     </center>
-
-     <div style={{textAlign:'right'}}>
-     <h2 style={{color:'red',fontSize:'120px'}}>Jesus 100px</h2>
-     <img src={img3} style={{width:'300px',borderRadius:'20%'}} />
-     </div>
-     <h2 style={{color:'blue'}}>Jesus 100px</h2> 
-     <img src={img3} style={{width:'350px', border:'20px solid green', borderRadius:'40%',float:'right'}} />
-      
-   </div>
+ <Home/>
+</div>
 
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import {Col,FormGroup,Input,Label,Row,ButtonDropdown,DropdownToggle,DropdownMenu,DropdownItem} from 'reactstrap';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                   <div className='text-center p-4 bg-primary'>
+       <h1 style={{color:'black'}}>Vidyajyoti Jesuit Residence</h1>
+       </div>
+       <FormGroup style={{overflow:"visible", paddingTop:'50px'}} >
+   <Row style={{marginBottom:'20px'}}>
+
+     <Col xs={3} lg={2}>
+     <Label for="username">Name:</Label>
+     </Col>
+     <Col xs={9} lg={4}>
+         <Input type="text"placeholder='Enter Your Name' value=""/>
+         </Col>
+         <Col xs={3} lg={2}>
+       <Label for="username">Password:</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+         <Input type="Password"placeholder='Enter Your Password' value=""/>
+         </Col>
+
+         </Row>
+
+         <Row style={{marginBottom:'20px'}}>
+
+     <Col xs={3} lg={2}>
+       <Label for="username">Email:</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+         <Input type="text"placeholder='Enter Your Email' value=""/>
+         </Col>
+         <Col xs={3} lg={2}>
+       <Label for="username">Phone Number:</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+         <Input type="text"placeholder='Enter Your Phone Number' value=""/>
+         </Col>
+
+         </Row>
+
+         <Row style={{marginBottom:'20px'}}>
+
+     <Col xs={3} lg={2}>
+       <Label for="province">Province:</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+         <Input type="text"placeholder='Enter Your Province' value=""/>
+         </Col>
+         <Col xs={3} lg={2}>
+       <Label for="dob">Date of Birth:</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+         <Input type="text" placeholder='Enter Your Date of Birth' value=""/>
+         </Col>
+
+         </Row>
+         <Row style={{marginBottom:'20px'}}>
+
+     <Col xs={3} lg={2}>
+       <Label for="blood">Blood Group:</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+       <Input type="text" placeholder='Enter Your Blood Group' value=""/>
+       </Col>
+       <Col xs={3} lg={2}>
+       <Label for="roomno">Room Number</Label>
+       </Col>
+       <Col xs={9} lg={4}>
+       <Input type="text" placeholder='Enter Your Room Number' value=""/>
+         </Col>
+
+         </Row>
+         </FormGroup>
+         <div className='taxt-center'>
+           {/*<button className='btn' style={{border: '2px solid blue'}}>submit</button>*/}
+           <button className='btn' style={{border: '2px solid blue',display:'none'}}>submit</button>
+</div>
+<ButtonDropdown
+  toggle={function noRefCheck(){}}
+>
+  <DropdownToggle caret>
+    Button Dropdown
+  </DropdownToggle>
+  <DropdownMenu>
+    <DropdownItem header>
+      Header
+    </DropdownItem>
+    <DropdownItem disabled>
+      Action
+    </DropdownItem>
+    <DropdownItem>
+      Another Action
+    </DropdownItem>
+    <DropdownItem divider />
+    <DropdownItem>
+      Another Action
+    </DropdownItem>
+  </DropdownMenu>
+</ButtonDropdown>
+  
+            </div>
+        )
+    }
+}
